3138545f3664956d5c5bed58349f6ed6871d21b5
Mofuyu <pangshengwei99.work@gmail.com>
2024-07-09 09:55:02 +0800

    fix misaligned height

--- Youtube-Chat-Resizer.user.js
+++ Youtube-Chat-Resizer.user.js
@@ -12,28 +12,29 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+	"use strict";
 
-  // GLOBAL VARIABLES
-  const querySelectors = {
-    css: "#YT-Chat-Resizer-Style",
-    header: "#masthead-container",
-    page_manager: "ytd-page-manager",
+	// GLOBAL VARIABLES
+	const querySelectors = {
+		css: "#YT-Chat-Resizer-Style",
+		header: "#masthead-container",
+		page_manager: "ytd-page-manager",
 
-    chat_container: "ytd-watch-flexy[fixed-panels] #chat.ytd-watch-flexy",
-    chat: "ytd-live-chat-frame",
+		chat_container:
+			"ytd-watch-flexy[fixed-panels] #chat.ytd-watch-flexy",
+		chat: "ytd-live-chat-frame",
 
-    video_container: ".html5-video-player > iframe",
-    video_thumbnail:
-      ".ytp-cued-thumbnail-overlay > .ytp-cued-thumbnail-overlay-image, .ytp-offline-slate > .ytp-offline-slate-background",
-    video_original: ".html5-video-container > video",
-  };
+		video_container: ".html5-video-player > iframe",
+		video_thumbnail:
+			".ytp-cued-thumbnail-overlay > .ytp-cued-thumbnail-overlay-image, .ytp-offline-slate > .ytp-offline-slate-background",
+		video_original: ".html5-video-container > video",
+	};
 
-  const desktopMinWidth = 1000,
-    headerHeight = 56,
-    emptySpaces = 105;
+	const desktopMinWidth = 1000,
+		headerHeight = 56,
+		emptySpaces = 105;
 
-  const css = `
+	const css = `
     :root {
       --pad-top: 20px;
     }
@@ -43,7 +44,7 @@
       height: calc(100% - var(--pad-top)) !important;
     }
     ${[querySelectors.video_thumbnail, querySelectors.video_original].join(
-      ", "
+		", "
     )} {
       visibility: hidden;
     }
@@ -67,91 +68,103 @@
     }
   `;
 
-  // HELPERS
-  let timer = null;
-  function debounce(fn, delay = 100) {
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        fn(...args);
-      }, delay);
-    };
-  }
+	// HELPERS
+	let timer = null;
+	function debounce(fn, delay = 100) {
+		return function (...args) {
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				fn(...args);
+			}, delay);
+		};
+	}
 
-  // FUNCTIONS
-  function injectCSS() {
-    const styleEl = document.createElement("style");
-    styleEl.id = querySelectors.css.substring(1);
-    styleEl.innerHTML = css;
+	// FUNCTIONS
+	function injectCSS() {
+		const styleEl = document.createElement("style");
+		styleEl.id = querySelectors.css.substring(1);
+		styleEl.innerHTML = css;
 
-    document.head.prepend(styleEl);
-  }
-  function onResizeChange() {
-    let frameStyle = ``;
-    if (window.outerWidth < desktopMinWidth) {
-      const videoPlayerHeight = ((window.outerWidth - 48) / 16) * 9;
+		document.head.prepend(styleEl);
+	}
+	function onResizeChange() {
+		let frameStyle = ``;
+		if (window.outerWidth < desktopMinWidth) {
+			let videoPlayerHeight =
+				((window.outerWidth - 48) / 16) * 9;
 
-      const maxHeight =
-        window.outerHeight - (emptySpaces - headerHeight + videoPlayerHeight);
-      const height = maxHeight - 24;
+			if (videoPlayerHeight <= 460) videoPlayerHeight = 460;
 
-      frameStyle = `min-height: 0; max-height: ${maxHeight}px; height: ${height}px; top: 0;`;
-    }
-    const targetEl = document.querySelector(querySelectors.chat);
-    if (typeof targetEl == "object" && targetEl !== null) {
-      targetEl.style = frameStyle;
-    }
-  }
+			const maxHeight =
+				window.outerHeight -
+				(emptySpaces -
+					headerHeight +
+					videoPlayerHeight);
+			const height = maxHeight - 24;
 
-  var removeAdsVideoPlayerInterval = null;
-  function removeAdsVideoPlayer() {
-    // Note: Will be remove once everything settled up, this function is temporarily assisting.
-    // [Remove Adblock Thing 5.5] is using iframe
+			frameStyle = `min-height: 0; max-height: ${maxHeight}px; height: ${height}px; top: 0;`;
+		}
+		const targetEl = document.querySelector(querySelectors.chat);
+		if (typeof targetEl == "object" && targetEl !== null) {
+			targetEl.style = frameStyle;
+		}
+	}
 
-    const el = document.querySelector("#primary .video-stream");
-    if (el && el?.remove) {
-      el.remove();
-      clearInterval(removeAdsVideoPlayerInterval);
-    }
-  }
+	var removeAdsVideoPlayerInterval = null;
+	function removeAdsVideoPlayer() {
+		// Note: Will be remove once everything settled up, this function is temporarily assisting.
+		// [Remove Adblock Thing 5.5] is using iframe
 
-  // ON DOCUMENT LOAD
-  function install() {
-    // Patch for Remove Adblock Thing 5.5
-    // removeAdsVideoPlayerInterval = setInterval(removeAdsVideoPlayer, 100);
+		const el = document.querySelector("#primary .video-stream");
+		if (el && el?.remove) {
+			el.remove();
+			clearInterval(removeAdsVideoPlayerInterval);
+		}
+	}
 
-    // Customisable, etc...
-    setTimeout(function () {
-      // Inject CSS
-      injectCSS();
+	// ON DOCUMENT LOAD
+	function install() {
+		// Patch for Remove Adblock Thing 5.5
+		// removeAdsVideoPlayerInterval = setInterval(removeAdsVideoPlayer, 100);
 
-      // Live Chat
-      onResizeChange();
-      window.addEventListener("resize", debounce(onResizeChange));
-    }, 3000);
-  }
-  function uninstall() {
-    setTimeout(function () {
-      document.head.removeChild(document.querySelector(querySelectors.css));
-      window.removeEventListener("resize");
-    }, 100);
-  }
-  function setup() {
-    if (window.location.pathname == "/watch") install();
-    else uninstall();
-  }
-  function StartPathListener() {
-    let previousPath = null;
-    setInterval(() => {
-      if (previousPath == null) previousPath = window.location.pathname;
-      else if (previousPath !== window.location.pathname) {
-        previousPath = window.location.pathname;
-        setup();
-      }
-    }, 500);
-  }
+		// Customisable, etc...
+		setTimeout(function () {
+			// Inject CSS
+			injectCSS();
 
-  // EXECUTION
-  setup();
-  StartPathListener();
+			// Live Chat
+			onResizeChange();
+			window.addEventListener(
+				"resize",
+				debounce(onResizeChange)
+			);
+		}, 3000);
+	}
+	function uninstall() {
+		setTimeout(function () {
+			document.head.removeChild(
+				document.querySelector(querySelectors.css)
+			);
+			window.removeEventListener("resize");
+		}, 100);
+	}
+	function setup() {
+		if (window.location.pathname == "/watch") install();
+		else uninstall();
+	}
+	function StartPathListener() {
+		let previousPath = null;
+		setInterval(() => {
+			if (previousPath == null)
+				previousPath = window.location.pathname;
+			else if (previousPath !== window.location.pathname) {
+				previousPath = window.location.pathname;
+				setup();
+			}
+		}, 500);
+	}
+
+	// EXECUTION
+	setup();
+	StartPathListener();
 })();
